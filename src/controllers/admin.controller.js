e899ddc3f26b57b5b331d528c9455b6d800e9a19
sherifbai <godness980@gmail.com>
2022-02-01 21:16:09 +0300

    done remove admin

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -24,6 +24,40 @@ exports.giveAdmin = async (req, res, next) => {
 
         await user.save();
 
+        user.password = null;
+
+        res.status(200).json({ user });
+    } catch (error) {
+
+    }
+}
+
+exports.removeAdmin = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const admin = await User.findById(req.userId);
+
+        if (!admin.isAdmin) {
+            const error = new Error('You are not admin');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        user.isAdmin = false;
+
+        await user.save();
+        
+        user.password = null;
+
         res.status(200).json({ user });
     } catch (error) {
 
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -6,5 +6,6 @@ const adminController = require('../controllers/admin.controller');
 const router = Router();
 
 router.post('/giveadmin/:id', isAuth, adminController.giveAdmin);
+router.post('/removeadmin/:id', isAuth, adminController.removeAdmin);
 
 module.exports = router;
